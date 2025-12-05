1be24c21cf6fcf10978a400fc36db59694b04272
ben143b <biguvubs@vcu.edu>
2025-12-04 19:31:14 -0500

    TV SCREEN Fixed!!!
    Works Finally!!!!!!

---
+++ GameFinal/Assets/Scenes/Benhi Scene/TheatherRoomTv/VideoScreenInteractable.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoScreenInteractable : Interactable, IInteractable
+{
+    [SerializeField] private VideoPlayer videoPlayer; // Reference to the VideoPlayer component
+    [SerializeField] private ItemType requiredItem = ItemType.Disc;
+
+    public void Interact(InventoryItem heldItem)
+    {
+        if (heldItem == null)
+        {
+            HintManager.Instance.ShowHint("You need a Disc to start the video.");
+            return;
+        }
+
+        if (heldItem.itemType != requiredItem)
+        {
+            HintManager.Instance.ShowHint("This item cannot be used here.");
+            return;
+        }
+
+        // Correct item → play video
+        videoPlayer.Play();
+
+        // Remove the disc from inventory
+        GameManager.Instance.RemoveSelectedItem();
+
+        // Optional: play sound
+        PlayUseAudio();
+    }
+}
